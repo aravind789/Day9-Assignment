0d809632d69d26e267948cb78e1488b76340a8a4
aravind789 <aravindkris89@gmail.com>
2020-07-02 14:51:58 +0530

    Wage for month

--- emp_wage_computation_prog.sh
+++ emp_wage_computation_prog.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
-random=$(($RANDOM%2))
 wage_per_hour=20
 is_part_time=1
 is_full_time=0
 work_hours=0
-case $random in
+Working_days_per_month=20
+n=0
+while [ $n -lt 20 ]
+do
+	random=$(($RANDOM%2))
+	case $random in
 	$is_part_time)  work_hours=4;;
 	$is_full_time)  work_hours=8;;
 	*)		work_hours=0;;
 esac
+n=$((n+1))
 dailyWage=$(($wage_per_hour*$work_hours))
 echo "DailyWage" $dailyWage
