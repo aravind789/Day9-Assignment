217fe95859a4b3c270e1694511ed1e956f048304
aravind789 <aravindkris89@gmail.com>
2020-07-02 13:12:27 +0530

    daily wage

--- emp_wage_computation_prog.sh
+++ emp_wage_computation_prog.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
 random=$(($RANDOM%2))
+
 if [ $random -eq 1 ]
 then
 	echo "Employee is present"
 else
 	echo "Employee is absent"
 fi
+wage_per_hour=20
+is_full_time=8
+dailyWage=$(($wage_per_hour*$is_full_time))
+
