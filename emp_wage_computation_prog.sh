817d0049409aca2467ed0d8e7df2d7853f1324c0
aravind789 <aravindkris89@gmail.com>
2020-07-02 15:02:04 +0530

    added condition

--- emp_wage_computation_prog.sh
+++ emp_wage_computation_prog.sh
@@ -4,9 +4,13 @@ is_part_time=1
 is_full_time=0
 work_hours=0
 Working_days_per_month=20
+max_hours_in_month=100
+total_working_days=0
+total_working_hours=0
 n=0
-while [ $n -lt 20 ]
+while [ $total_working_hours -lt $max_hours_in_month ] && [  $total_working_days -lt $Working_days_per_month ]
 do
+	((total_working_days))
 	random=$(($RANDOM%2))
 	case $random in
 	$is_part_time)  work_hours=4;;
@@ -15,4 +19,5 @@ do
 esac
 n=$((n+1))
 dailyWage=$(($wage_per_hour*$work_hours))
-echo "DailyWage" $dailyWage
+echo "DailyWage is :" $dailyWage
+done
